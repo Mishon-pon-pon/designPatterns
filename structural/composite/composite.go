bcb103d2cad8228483b814b6817a11eb31d32ed1
Mishon-pon-pon <terem848@gmail.com>
2020-02-03 15:53:14 +0300

    review composite

--- structural/composite/composite.go
+++ structural/composite/composite.go
@@ -1,26 +1,33 @@
 package composite
 
+// Athlete ...
 type Athlete struct{}
 
+// Train ...
 func (a *Athlete) Train() string {
 	return "Training!"
 }
 
+// ComopositeSwimmerA ...
 type ComopositeSwimmerA struct {
 	MyAthlete Athlete
 	MySwim    func() string
 }
 
+// Swim ...
 func Swim() string {
 	return "Swimming!"
 }
 
+// Animal ...
 type Animal struct{}
 
+// Eat ...
 func (a *Animal) Eat() string {
 	return "Eating!"
 }
 
+// Shark ...
 type Shark struct {
 	Animal
 	Swim func() string
